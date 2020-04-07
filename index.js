4d337b7fdf76a56f62529c6a57b2f22f5aac25ea
intanadhella <intanadhelia655@gmail.com>
2020-04-07 17:21:35 +0700

    nambahin kota

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const URL = 'http://api.openweathermap.org/data/2.5/weather?q=Jakarta,ID&units=metric&appid=08c63bb12c5a4132f5d570f08f17872d'
+const URL = 'http://api.openweathermap.org/data/2.5/group?id=1642911,1214520,1642858,1631761&units=metric&appid=08c63bb12c5a4132f5d570f08f17872d'
 fetch(URL)
 .then((response) => {
     return response.json();
@@ -19,30 +19,52 @@ fetch(URL)
 //     `;
 //     document.getElementById('weather').innerHTML = weatherHTMLString;
 // }) 
-
+.then (data => {
+    return data.list
+})
 .then (data => {
     console.log(data)
-    const {
-        weather,
-        name,
-        main
-    } = data
-    const weatherHTMLString = `
-    <div class="col-sm-6 col-md-4">
-        weather : ${weather[0]}
-        <br>
-        name : ${name}
-    </div>
-    `;
-    document.getElementById('weather').innerHTML = weatherHTMLString;
+
+    //currentCity
+    const currentCityHead = document.getElementById('currentCity-head')
+    currentCityHead.innerHTML = data[0].name
+    const date = new Date()
+    const currentCityDate = document.getElementById('currentCity-date')
+    currentCityDate.innerHTML = date
+    const currentCityIcon = document.getElementById('currentCity-icon')
+    currentCityIcon.src = `http://openweathermap.org/img/wn/${data[0].weather[0].icon}@2x.png`
+    currentCityIcon.alt = data[0].weather[0].description
     
-    document.getElementsByClassName('location')[0].getElementsByTagName('p')[0].innerHTML = name
-    document.getElementsByClassName('temperature-description')[0].getElementsByTagName('p')[0].innerHTML = `${weather[0].main} (${weather[0].description})`
-    document.getElementsByClassName('temperature-value')[0].getElementsByTagName('p')[0].innerHTML = `${main.temp} °C`
-    let icon = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`
-    document.getElementsByClassName('weather-icon')[0].getElementsByTagName('img')[0].src = icon
-
-
-    document.getElementById('weather').innerHTML = `<h2>${data.weather[0].main}</h2>`
-    document.getElementsByTagName
+    //secondCity
+    const secondCityHead = document.getElementById('secondCity-head')
+    secondCityHead.innerHTML = data[1].name
+    const secondCityIcon = document.getElementById('secondCity-icon')
+    secondCityIcon.src = `http://openweathermap.org/img/wn/${data[1].weather[0].icon}@2x.png`
+    secondCityIcon.alt = data[1].weather[0].description
+    const secondCityTemp = document.getElementById('secondCity-temp')
+    secondCityTemp.innerHTML = data[1].main.temp
+    const secondCityDesc = document.getElementById('secondCity-desc')
+    secondCityDesc.innerHTML = data[1].weather[0].main
+    
+    //thirdCity
+    const thirdCityHead = document.getElementById('thirdCity-head')
+    thirdCityHead.innerHTML = data[2].name
+    const thirdCityIcon = document.getElementById('thirdCity-icon')
+    thirdCityIcon.src = `http://openweathermap.org/img/wn/${data[2].weather[0].icon}@2x.png`
+    thirdCityIcon.alt = data[2].weather[0].description
+    const thirdCityTemp = document.getElementById('thirdCity-temp')
+    thirdCityTemp.innerHTML = data[2].main.temp
+    const thirdCityDesc = document.getElementById('thirdCity-desc')
+    thirdCityDesc.innerHTML = data[2].weather[0].main
+    
+    //fourthCity
+    const fourthCityHead = document.getElementById('fourthCity-head')
+    fourthCityHead.innerHTML = data[3].name
+    const fourthCityIcon = document.getElementById('fourthCity-icon')
+    fourthCityIcon.src = `http://openweathermap.org/img/wn/${data[3].weather[0].icon}@2x.png`
+    fourthCityIcon.alt = data[3].weather[0].description
+    const fourthCityTemp = document.getElementById('fourthCity-temp')
+    fourthCityTemp.innerHTML = data[3].main.temp
+    const fourthCityDesc = document.getElementById('fourthCity-desc')
+    fourthCityDesc.innerHTML = data[3].weather[0].main
 }) 
