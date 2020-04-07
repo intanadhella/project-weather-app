8a0b807b489d2ac3a273f9e0ef01bfa0f6b95d78
intanadhella <intanadhelia655@gmail.com>
2020-04-07 15:51:52 +0700

    menambahkan icon dll

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const URL = 'http://api.openweathermap.org/data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d'
+const URL = 'http://api.openweathermap.org/data/2.5/weather?q=Jakarta,ID&units=metric&appid=08c63bb12c5a4132f5d570f08f17872d'
 fetch(URL)
 .then((response) => {
     return response.json();
 })
 .then (data => {
     console.log(data)
-    console.log(data);
     const {
         weather,
-        name
+        name,
+        main
     } = data
     const weatherHTMLString = `
     <div class="col-sm-6 col-md-4">
@@ -18,4 +18,11 @@ fetch(URL)
     </div>
     `;
     document.getElementById('weather').innerHTML = weatherHTMLString;
+    
+    document.getElementsByClassName('location')[0].getElementsByTagName('p')[0].innerHTML = name
+    document.getElementsByClassName('temperature-description')[0].getElementsByTagName('p')[0].innerHTML = `${weather[0].main} (${weather[0].description})`
+    document.getElementsByClassName('temperature-value')[0].getElementsByTagName('p')[0].innerHTML = `${main.temp} °C`
+    let icon = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`
+    document.getElementsByClassName('weather-icon')[0].getElementsByTagName('img')[0].src = icon
+
 }) 
