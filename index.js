f6c223d250b14068ac6a95235179b90c755665ff
ferdinandw <fwng123@gmail.com>
2020-04-07 15:32:57 +0700

    fetch

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const URL = 'http://api.openweathermap.org/data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d'
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=Jakarta,ID&appid=08c63bb`).then((response) => {
-
+fetch(URL)
+.then((response) => {
     return response.json();
 })
 .then (data => {
