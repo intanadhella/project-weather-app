30dacb637c24ff9dfd828982e3ea25c3e781550c
Ahmad Fakhrozy <ahmadfakhrozy@gmail.com>
2020-04-07 14:19:24 +0700

    add fetch

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const URL = 'http://api.openweathermap.org/data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d'
+
+fetch(`http://api.openweathermap.org/data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d`)
+.then (response => {
+    return response.json();
+})
+.then (data => {
+    console.log(data)
+})
